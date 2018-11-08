ee31c6fe71cf45de2e14cf7512b517ad28684f5d
Kerstine Rodilla <kerstinerodilla@gmail.com>
2018-11-08 14:40:45 +0800

    added input text error

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@
  	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
  	var errorEmail = document.querySelector(".error-email");
  	var password = form.password.value;
+ 	var passwordInput = document.getElementById('password');
  	var errorPassword = document.querySelector(".error-password");
  	var password2 = form.password2.value;
+ 	var password2Input = document.getElementById('password2');
  	var errorPassword2 = document.querySelector(".error-password2");
 
  	
@@ -14,6 +16,7 @@
  		errorEmail.style.display='inline';
  		errorEmail.innerHTML = 'This field is required!';
  		errorEmail.style.color = 'red';
+ 		textEmail.classList.add('error-input-box');
  	} else{
  		 errorEmail.style.display='none';
  	}
@@ -22,6 +25,7 @@
  		errorPassword.style.display='inline';
  		errorPassword.innerHTML = "This field is required!";
  		errorPassword.style.color = 'red';
+ 		passwordInput.classList.add('error-input-box');
  	} else{
  		 errorPassword.style.display='none';
  	}
@@ -30,6 +34,7 @@
  		errorPassword2.style.display='inline';
  		errorPassword2.innerHTML = "This field is required!";
  		errorPassword2.style.color = 'red';
+ 		password2Input.classList.add('error-input-box');
  	} else{
  		 errorPassword2.style.display='none';
  	}
@@ -38,6 +43,7 @@
 	   	errorEmail.innerHTML = 'Invalid Email';
 	   	errorEmail.style.color = 'red';
 	   	errorEmail.style.display = 'inline';
+	   	textEmail.classList.add('error-input-box');
 	    textEmail.focus;
 	}
 
@@ -53,6 +59,8 @@
 			errorPassword2.innerHTML = "Didn't matched!";
 			errorPassword.style.color = 'red';
 			errorPassword2.style.color = 'red';
+			passwordInput.classList.add('error-input-box');
+			
 	  	}
 	}
  	return false;
